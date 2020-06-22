d76eed8923461a0920a3b9be08ca87d21625c486
Henry Chang <henry5988@hotmail.com>
2020-06-22 08:45:03 -0400

    clean commit

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>{{testdata}}</div>
+</template>
+
+<script>
+    export default {
+        name: "Test",
+        props:{
+            testdata: String
+        },
+        data:function(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
